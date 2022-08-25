72a3554ddc35f27952ea0847bc7bcd3bda271767
Naqij <naqi.jaffari@gmail.com>
2022-08-25 20:46:07 +0000

    added what is required to create hangman

--- hangman.py
+++ hangman.py
@@ -1,4 +1,9 @@
+import random
 import requests
 
 response = requests.get("https://random-word-api.herokuapp.com/all")
 print(response.text)
+
+#import random word using API
+#input user word choice
+#5 lives
