7d33e448e586ce82d4259951bc11b63687afc83f
Naqij <naqi.jaffari@gmail.com>
2022-08-26 18:39:32 +0000

    added word library to choose words from

--- hangman.py
+++ hangman.py
@@ -1,9 +1,4 @@
 import random
-import requests
+from words import words_list
 
-response = requests.get("https://random-word-api.herokuapp.com/all")
-print(response.text)
-
-#import random word using API
-#input user word choice
-#5 lives
+print(words_list)
