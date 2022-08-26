560459e24969ba24a601a9688e3f93f60163672f
Naqij <naqi.jaffari@gmail.com>
2022-08-26 19:13:52 +0000

    added hangman design for the game

--- hangman.py
+++ hangman.py
@@ -1,4 +1,74 @@
 import random
 from words import words_list
 
-print(words_list)
+def get_word(words_list):
+    word = random.choice(words_list)
+    while '_' in word or ' ' in word:
+        word = random.choice(words_list)
+
+    return word
+
+def print_hangman(wrong):
+    if(wrong == 0):
+        print("\n----------")
+        print("   |")
+        print("   |")
+        print("   |")
+        print("   |")
+        print("  ---")
+
+    elif(wrong == 1):
+        print("\n-------+--")
+        print("   |     O")
+        print("   |")
+        print("   |")
+        print("   |")
+        print("  ---")
+
+    elif(wrong == 2):
+        print("\n-------+--")
+        print("   |     O")
+        print("   |    /")
+        print("   |")
+        print("   |")
+        print("  ---")
+
+    elif(wrong == 3):
+        print("\n-------+--")
+        print("   |     O")
+        print("   |    /")
+        print("   |")
+        print("   |")
+        print("  ---")
+
+    elif(wrong == 4):
+        print("\n-------+--")
+        print("   |     O")
+        print("   |    /|")
+        print("   |")
+        print("   |")
+        print("  ---")
+
+    elif(wrong == 5):
+        print("\n-------+--")
+        print("   |     O")
+        print("   |    /|\ ")
+        print("   |")
+        print("   |")
+        print("  ---")
+
+    elif(wrong == 6):
+        print("\n-------+--")
+        print("   |     O")
+        print("   |    /|\ ")
+        print("   |    /")
+        print("   |")
+        print("  ---")
+
+    elif(wrong == 7):
+        print("\n-------+--")
+        print("   |     O")
+        print("   |    /|\ ")
+        print("   |    / \ ")
+        print("   |")
+        print("  ---")
