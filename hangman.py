cf22b3c72ccb5e920561b665049363c8719ecdfe
Naqij <naqi.jaffari@gmail.com>
2022-08-28 11:06:11 +0000

    added random list for words and choose word randomly. clear

--- hangman.py
+++ hangman.py
@@ -2,76 +2,10 @@ import random
 from words import words_list
 
 def hangman():
-    
-def get_word(words):
     word = random.choice(words_list)
+    while '-' in word or ' ' in word:
+        word = random.choice(words_list)
+
     print(word)
-
-def print_hangman(wrong):
-    if(wrong == 0):
-        print("\n----------")
-        print("   |")
-        print("   |")
-        print("   |")
-        print("   |")
-        print("  ---")
-
-    elif(wrong == 1):
-        print("\n-------+--")
-        print("   |     O")
-        print("   |")
-        print("   |")
-        print("   |")
-        print("  ---")
-
-    elif(wrong == 2):
-        print("\n-------+--")
-        print("   |     O")
-        print("   |    /")
-        print("   |")
-        print("   |")
-        print("  ---")
-
-    elif(wrong == 3):
-        print("\n-------+--")
-        print("   |     O")
-        print("   |    /")
-        print("   |")
-        print("   |")
-        print("  ---")
-
-    elif(wrong == 4):
-        print("\n-------+--")
-        print("   |     O")
-        print("   |    /|")
-        print("   |")
-        print("   |")
-        print("  ---")
-
-    elif(wrong == 5):
-        print("\n-------+--")
-        print("   |     O")
-        print("   |    /|\ ")
-        print("   |")
-        print("   |")
-        print("  ---")
-
-    elif(wrong == 6):
-        print("\n-------+--")
-        print("   |     O")
-        print("   |    /|\ ")
-        print("   |    /")
-        print("   |")
-        print("  ---")
-
-    elif(wrong == 7):
-        print("\n-------+--")
-        print("   |     O")
-        print("   |    /|\ ")
-        print("   |    / \ ")
-        print("   |")
-        print("  ---")
-
-name = input("Enter your name please >> ")
 
 hangman()
