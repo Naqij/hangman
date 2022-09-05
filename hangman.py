5d9dcc6e38e4832d91270bb49bc5e6dccfce3a3c
Naqij <naqi.jaffari@gmail.com>
2022-09-05 17:06:57 +0000

    Added hangman design for the game (I already worked offline on this)

--- hangman.py
+++ hangman.py
@@ -16,3 +16,72 @@ def update():
     print()
 
 update()
+
+# Hangman to be printed if wrong answers are given
+def hangman(tries):
+    if (tries == 1):
+        print("7 tries left")
+        print("-+-----------")
+        print(" |    O      ")
+        print(" |           ")
+        print(" |           ")
+        print("===          ")
+
+    elif (tries == 2):
+        print("6 tries left")
+        print("-+-----------")
+        print(" |   O       ")
+        print(" |   |       ")
+        print(" |           ")
+        print("===          ")
+      
+    elif (tries == 3):
+        print("5 tries left")
+        print("-+-----------")
+        print(" |   O       ")
+        print(" |   |       ")
+        print(" |  /        ")
+        print("===          ")
+      
+    elif (tries == 4):
+        print("4 tries left")
+        print("-+-----------")
+        print(" |   O       ")
+        print(" |   |       ")
+        print(" |  / \      ")
+        print("===          ")
+
+    elif (tries == 5):
+        print("3 tries left")
+        print("-+-----------")
+        print(" |  \O       ")
+        print(" |   |       ")
+        print(" |  / \      ")
+        print("===          ")
+      
+    elif (tries == 6):
+        print("2 tries left")
+        print("-+-----------")
+        print(" |  \O/      ")
+        print(" |   |       ")
+        print(" |  / \      ")
+        print("===          ")
+
+    elif (tries == 7):
+        print("1 try left")
+        print("-+-----------")
+        print(" |   |       ")
+        print(" |  \O/      ")
+        print(" |   |       ")
+        print(" |  / \      ")
+        print("===          ")
+      
+    elif (tries == 8):
+        print("-+---+-------")
+        print(" |   |       ")
+        print(" |   +O      ")
+        print(" | / | \     ")
+        print(" |  / \      ")
+        print("===          ")
+
+
