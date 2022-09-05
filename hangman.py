dec91462cc7e3eb3441442242b184327709cbf2d
Naqij <naqi.jaffari@gmail.com>
2022-09-05 17:04:38 +0000

    Added alphabet for correcting the word

--- hangman.py
+++ hangman.py
@@ -4,7 +4,15 @@ from words import word_list
 # This section will import a random word from words.py
 word = random.choice(word_list)
 
+# These will collect number of correct, incorrect etc
 correct = []
 incorrect = []
 letters_guessed = []
 current_guessed = 0
+
+def update():
+    for x in correct:
+        print(x, end=' ')
+    print()
+
+update()
